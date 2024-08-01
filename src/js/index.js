519f33f249ef342f9fa9a2b8a926816a5fe98b83
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-01 14:50:30 +0000

    "Traffic-light-Alec"

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "/workspaces/Traffic-light-Alec/src/styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import TrafficLight from "./component/TrafficLight.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.createRoot(document.getElementById('app')).render(<TrafficLight/>);
 
